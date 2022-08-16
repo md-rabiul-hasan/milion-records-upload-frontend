c1579190639085af8e11a1b6119ee2bcf9f24180
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-08-16 23:18:47 +0600

    show progress bar

--- pages/upload.js
+++ pages/upload.js
@@ -6,6 +6,7 @@ export default function upload() {
   const fileRef      = useRef();
   const API_ENDPOINT = "http://127.0.0.1:8000/api/v1";
   const [batchDetails, setBatchDetails] = useState({});
+  const [batchId, setBatchId] = useState(null);
 
   const handleForm = (e) => {
     e.preventDefault();
@@ -18,36 +19,45 @@ export default function upload() {
     fromData.append("mycsv", file);
     fetch(`${API_ENDPOINT}/upload`, { method: "post", body: fromData })
         .then( (res) => res.json())
-        .then( (data) => console.log(data))
+        .then( (data) => {
+          console.log(data);
+          setBatchId(data.id);
+          fetchBatchDetails(data.id);
+        })
 
   }
 
-  const fetchBatchDetails = () => {
-    const batchId = "9708f9f8-5937-46b6-8684-d439aa63802a";
-    fetch(`${API_ENDPOINT}/batch/${batchId}`)
+  const fetchBatchDetails = (id = null) => {
+    const currentBatchId = id ?? batchId;
+    console.log(currentBatchId);
+    fetch(`${API_ENDPOINT}/batch/${currentBatchId}`)
         .then( (res) => res.json())
         .then( (data) => setBatchDetails(data))
   }
 
   useEffect( () => {
-    fetchBatchDetails();
+    setInterval( () => {
+      if(batchDetails.progress && batchDetails.progress !== 100){
+        fetchBatchDetails();
+      }
+    }, 2000);
   }, []);
 
   return (
     <Layout>
-      {batchDetails && 
+      {batchDetails.progress && 
         <section>
           <h1 className='text-xl text-gray-800 text-center mb-5 font-bold'>File uploading.... ({batchDetails.progress}%)</h1>
           <progress value={batchDetails.progress} max="100"></progress>
         </section>
       }
 
-    { !batchDetails && 
+    { !batchDetails.progress && 
       <section>
         <h1 className='text-xl text-gray-800 text-center mb-5 font-bold'>Choose a million record file to upload</h1>
         <form className='border rounded p-4' onSubmit={handleForm}>
           <input type="file" ref={fileRef}/>
-          <input type="submit" value="Upload" className="px-4 py-2 bg-gray-700 rounded text-white" />
+          <input type="submit" value="Upload" className="px-4 py-2 bg-gray-700 rounded text-white bg-indigo-500" />
         </form>
       </section>
     }
